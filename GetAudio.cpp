1de6bdc5924bb11b217b8d6f3784ba68a28a26d0
yannick.daelemans <yannick.daelemans@student.kuleuven.be>
2020-10-11 14:49:07 +0200

    First Commit

---
+++ GetAudio.cpp
@@ -0,0 +1,10 @@
+#include <windows.h>
+#include <Audiopolicy.h>
+#include <Mmdeviceapi.h>
+#include <stdio.h>
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+  std::cout << "Hello World";
+  return 0;
+}
